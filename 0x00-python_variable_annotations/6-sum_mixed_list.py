bc1de0183de53d7c54c74e6f0d577553e2fdef7a
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-11 07:22:03 +0300

    t6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Basic Annotations"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """sum_mixed_list function"""
+    return sum(mxd_lst)
