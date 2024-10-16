6eba9b54a00596b892b1c435af04ad95e167c305
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-16 05:51:38 +0300

    update

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,5 +4,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
+    """coroutine will collect 10 random numbers & return the 10 random numbers"""
     random_num = [randomNum async for randomNum in async_generator()]
     return random_num
