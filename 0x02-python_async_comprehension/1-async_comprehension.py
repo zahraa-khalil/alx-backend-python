c7ccfc91ce86532867302f00b816a3f32ef8f2f0
zahraa-khalil <zahraa.khalil2020@gmail.com>
2024-10-16 05:47:49 +0300

    update

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,3 +5,8 @@ import random
 from typing import AsyncGenerator
 
 async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension():
+    random_num = [randomNum async for randomNum in async_generator()]
+    return random_num
